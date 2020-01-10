1541234d6ddcaeaf1666875478b8885569287759
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-01-09 23:30:44 -0300

    Edit main respository to fetch repository and add to state

--- react-project-02/src/pages/Main/styles.js
+++ react-project-02/src/pages/Main/styles.js
@@ -1,22 +1,6 @@
-import styled from 'styled-components';
+import styled, {keyframes, css } from 'styled-components';
 
-export const Container = styled.div`
-  max-width: 700px;
-  background: #FFF;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  margin 80px; auto;
 
-  h1 {
-    font-size: 20 px;
-    display: flex;
-    flex-direction: row;
-  }
-
-  svg {
-    margin-right: 10px;
-  }
-`;
 
 export const Form = styled.form`
   margin-top: 30px;
@@ -25,14 +9,25 @@ export const Form = styled.form`
 
   input {
     flex: 1;
-    border: 1px; solid #eee;
+    border: 1px solid #eee;
     padding: 10px 15px;
     border-radius: 4px;
     font-size: 16px;
   }
 `;
 
-export const SubmitButton = styled.button`
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
+`;
+export const SubmitButton = styled.button.attrs(props => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -42,4 +37,34 @@ export const SubmitButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disable] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+  ${props => props.loading && css`
+    svg{
+      animation: ${rotate} 2s linear infinite;
+    }
+  `}
 `;
+
+export const List = styled.ul`
+  list-style:none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  a{
+    color: #7159c1;
+    text-decoration: none;
+    
+  }
+`;
+
